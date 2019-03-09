7447ebb385e5aaeb832b61b1600aef1aa8ab0049
Ваше Имя <anna.ko4523546@gmail.com>
2019-03-09 22:46:30 +0100

    1 commit

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+  <header class="main-header">
+    <div class="main-nav">
+      <ul class="nav-links">
+        <nuxt-link
+          to="/"
+          tag="li"><a>All Posts</a></nuxt-link>
+        <nuxt-link
+          to="about" 
+          tag="li"><a>About</a></nuxt-link>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+  .main-header
+    position: fixed;
+    top: 0
+    left: 0
+    width: 100%
+    background: #022d30
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <TheHeader/>
+    <main class="main-content">
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script type="text/javascript">
+import TheHeader from '@/components/TheHeader/TheHeader'
+export default {
+  components: {
+    TheHeader
+  }
+}
+</script>
+
+<style lang="stylus">
+  *
+    box-sizing: border-box;
+body
+    font-family: ZCOOL QingKe HuangYou, sans-serif;
+
+.main-content
+    margin-top: 4.5rem
+</style>
